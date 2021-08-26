5171281b7dde06e9e96f77451a4b32b5d62d3283
akramksb <akram.kssiba@gmail.com>
2021-08-26 15:33:12 +0100

    front-end addUser

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,10 +1,14 @@
 const usernameText = document.querySelector('#username-nav')
 
 
+
 async function getUserInfo()
 {
     const response = await fetch("/users/current")
-    const data = await response.json()
-    usernameText.textContent = data.username
-} 
+    const user = await response.json()
+    if (user.role !== "admin")
+        location.assign('/login')
+    usernameText.textContent = user.username
+}
+
 getUserInfo()
